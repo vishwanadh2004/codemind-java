cc6978fb16464f0e351dcd1554f1744ea67b5204
vishwanadh2004 <119720593+vishwanadh2004@users.noreply.github.com>
2023-05-23 19:05:27 +0530

    import java.util.Scanner; public class Sample {     public static void main(String args[])throws Exception     {       int a,b,c;       float s,area;       Scanner sc=new Scanner(System.in); 	  a=sc.nextInt(); 	  b=sc.nextInt(); 	  c=sc.nextInt();       s=(float)(a+b+c)/2;       area=(float)Math.sqrt(s*(s-a)*(s-b)*(s-c));       System.out.format("%.2f",area);     } }

---
+++ Area_of_the_Triangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Sample
+{
+    public static void main(String args[])throws Exception
+    {
+      int a,b,c;
+      float s,area;
+      Scanner sc=new Scanner(System.in);
+	  a=sc.nextInt();
+	  b=sc.nextInt();
+	  c=sc.nextInt();
+      s=(float)(a+b+c)/2;
+      area=(float)Math.sqrt(s*(s-a)*(s-b)*(s-c));
+      System.out.format("%.2f",area);
+    }
+} 
