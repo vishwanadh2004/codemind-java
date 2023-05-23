e965a3c74ca6f48c2e185e30da7b74c74eec421f
vishwanadh2004 <119720593+vishwanadh2004@users.noreply.github.com>
2023-05-23 20:21:38 +0530

    import java.io.*; import java.util.Scanner; public class Sample{     public static void main(String args[])     {         int a,b,multiply;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         multiply=a*b;         System.out.println(multiply);     } }

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Scanner;
+public class Sample{
+    public static void main(String args[])
+    {
+        int a,b,multiply;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        multiply=a*b;
+        System.out.println(multiply);
+    }
+}
