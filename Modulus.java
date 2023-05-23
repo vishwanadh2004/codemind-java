535e1742e2e12b10e662d4d2319aa1a3a4ef8f04
vishwanadh2004 <119720593+vishwanadh2004@users.noreply.github.com>
2023-05-23 20:16:25 +0530

    import java.io.*; import java.util.Scanner; public class Sample {     public static void main(String args[])     {      int a,b,c;      Scanner sc=new Scanner(System.in);      a=sc.nextInt();      b=sc.nextInt();      c=a%b;      System.out.println(c);     } }

---
+++ Modulus.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+public class Sample
+{
+    public static void main(String args[])
+    {
+     int a,b,c;
+     Scanner sc=new Scanner(System.in);
+     a=sc.nextInt();
+     b=sc.nextInt();
+     c=a%b;
+     System.out.println(c);
+    }
+}
