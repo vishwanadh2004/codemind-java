573d44d01d0ab61f5c9dc25f0ab7108f61d26618
vishwanadh2004 <119720593+vishwanadh2004@users.noreply.github.com>
2023-06-02 07:48:42 +0530

    import java.util.Scanner; public class codemind {     public static void main(String args[])     {         int a;         Scanner sc=new Scanner(System.in);         a=sc.next().charAt(0);         System.out.println((int)a);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; 
+public class codemind
+{
+    public static void main(String args[]) 
+    {
+        int a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.next().charAt(0);
+        System.out.println((int)a);     
+    }
+}
