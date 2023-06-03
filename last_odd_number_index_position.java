353683766053f95448c820330ea204421ef68419
vishwanadh2004 <119720593+vishwanadh2004@users.noreply.github.com>
2023-06-03 17:10:00 +0530

    import java.util.*; public class Codemind {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,c=0;         n=sc.nextInt();         int[] a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();             if(a[i]%2!=0)             {                 c=i;             }         }         System.out.printf("%d",c);     } }

---
+++ last_odd_number_index_position.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Codemind
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,c=0;
+        n=sc.nextInt();
+        int[] a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            if(a[i]%2!=0)
+            {
+                c=i;
+            }
+        }
+        System.out.printf("%d",c);
+    }
+}
