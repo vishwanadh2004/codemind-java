2f4a0118ee6e4c413331d14fa2da597586dfe9bc
vishwanadh2004 <119720593+vishwanadh2004@users.noreply.github.com>
2023-06-03 16:49:18 +0530

    import java.util.*; public class Codemind {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int i,n,c=0,t,j,k;         n=sc.nextInt();         t=n;         int[] a= new int[n];         int[] b= new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();             b[i]=a[i];         }         for(i=0;i<n;i++)         {             for(j=i+1;j<n;j++)             {                 if(a[i]==a[j])                 {                     for(k=j;k<n-1;k++)                     {                         a[k]=a[k+1];                     }                     j--;                     n--;                 }             }         }         for(i=0;i<n;i++)         {             c=0;           for(j=0;j<t;j++)           {               if(a[i]==b[j])               {                   c++;               }           }           System.out.printf("%d %d ",a[i],c);         }     } }

---
+++ Element_and_Count.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class Codemind
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int i,n,c=0,t,j,k;
+        n=sc.nextInt();
+        t=n;
+        int[] a= new int[n];
+        int[] b= new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            b[i]=a[i];
+        }
+        for(i=0;i<n;i++)
+        {
+            for(j=i+1;j<n;j++)
+            {
+                if(a[i]==a[j])
+                {
+                    for(k=j;k<n-1;k++)
+                    {
+                        a[k]=a[k+1];
+                    }
+                    j--;
+                    n--;
+                }
+            }
+        }
+        for(i=0;i<n;i++)
+        {
+            c=0;
+          for(j=0;j<t;j++)
+          {
+              if(a[i]==b[j])
+              {
+                  c++;
+              }
+          }
+          System.out.printf("%d %d ",a[i],c);
+        }
+    }
+}
