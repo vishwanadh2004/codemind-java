00a71d81d04d54a383eec8a25b701cac8e67394c
vishwanadh2004 <119720593+vishwanadh2004@users.noreply.github.com>
2023-06-03 17:26:21 +0530

    import java.util.Scanner; public class Codemind {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();        int c=0;        for(int i=a;i<=b;i++)        {            if(i==1)            {                continue;            }            else            {                int d=0;                for(int j=2;j*j<=i;j++)                {                    if(i%j==0)                    {                        d+=1;                    }                }                if(d==0)                {                    c+=1;                }            }        }        System.out.println(c);     } }

--- Primes_in_range.java
+++ Primes_in_range.java
@@ -1,35 +1,34 @@
 import java.util.Scanner;
-public class Codemind 
+public class Codemind
 {
-    public static void main(String args[]) 
+    public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
-        int b=sc.nextInt(); 
-        int c=0;    
-        for(int i=a;i<=b;i++)
-        {
-            if(i==1)        
-            { 
-                continue;
-                }
-                else    
-                { 
-                    int d=0;
-                    for(int j=2;j*j<=i;j++) 
-                    {
-                        if(i%j==0)
-                        {   
-                            d+=1;  
-                        }           
-                    }
-                            if(d==0)        
-                            {
-                                c+=1;
-                            }    
-                }
-            }
-                                System.out.println(c);
-        }
+        int b=sc.nextInt();
+       int c=0;
+       for(int i=a;i<=b;i++)
+       {
+           if(i==1)
+           {
+               continue;
+           }
+           else
+           {
+               int d=0;
+               for(int j=2;j*j<=i;j++)
+               {
+                   if(i%j==0)
+                   {
+                       d+=1;
+                   }
+               }
+               if(d==0)
+               {
+                   c+=1;
+               }
+           }
+       }
+       System.out.println(c);
     }
-                                
+}
