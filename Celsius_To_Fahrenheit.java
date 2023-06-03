30da7c983d625364133221c8a810bc7489bb7330
vishwanadh2004 <119720593+vishwanadh2004@users.noreply.github.com>
2023-06-03 17:21:54 +0530

    import java.util.*; public class Codemind {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a;         a=sc.nextInt();         double b=a*1.8+32;         System.out.printf("%.2f",b); } }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Codemind
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a;
+        a=sc.nextInt();
+        double b=a*1.8+32;
+        System.out.printf("%.2f",b);
+}
+}
