5d7b9263f9dc652a6cf9148d365e740927a42920
vishwanadh2004 <119720593+vishwanadh2004@users.noreply.github.com>
2023-06-03 16:43:46 +0530

    import java.util.*; public class Codemind {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,c=0,count=0,j;         n=sc.nextInt();         int[] a=new int[n];         for(i=0;i<n;i++)         {         a[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             c=0;             for(j=i+1;j<n;j++)             {                 if(a[i]==a[j])                 {                     c++;                 }             }             if(c==0 && a[i]%2==0)             {                 count++;             }         }         System.out.println(count);         sc.close();     } }

---
+++ unique_even_count.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Codemind
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,c=0,count=0,j;
+        n=sc.nextInt();
+        int[] a=new int[n];
+        for(i=0;i<n;i++)
+        {
+        a[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            c=0;
+            for(j=i+1;j<n;j++)
+            {
+                if(a[i]==a[j])
+                {
+                    c++;
+                }
+            }
+            if(c==0 && a[i]%2==0)
+            {
+                count++;
+            }
+        }
+        System.out.println(count);
+        sc.close();
+    }
+}
