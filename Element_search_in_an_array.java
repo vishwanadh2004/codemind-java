3ba0679af17866e43fbe87beb0567837cdbe0d6b
vishwanadh2004 <119720593+vishwanadh2004@users.noreply.github.com>
2023-06-03 17:11:58 +0530

    import java.util.*; public class Codemind {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,c=0,b;         n=sc.nextInt();         int[] a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         b=sc.nextInt();         for(i=0;i<n;i++)         {             if(a[i]==b)             {                 c++;                 break;             }         }         if(c!=0)         System.out.printf("True");         else         System.out.printf("False");         sc.close();     } }

---
+++ Element_search_in_an_array.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Codemind
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,c=0,b;
+        n=sc.nextInt();
+        int[] a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        b=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(a[i]==b)
+            {
+                c++;
+                break;
+            }
+        }
+        if(c!=0)
+        System.out.printf("True");
+        else
+        System.out.printf("False");
+        sc.close();
+    }
+}
