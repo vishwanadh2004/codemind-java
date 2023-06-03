cca7f60afad85ba658c4009b1cad5787a8078394
vishwanadh2004 <119720593+vishwanadh2004@users.noreply.github.com>
2023-06-03 16:34:43 +0530

    import java.util.*; public class Codemind {     public static void main(String args[])     {       Scanner sc=new Scanner(System.in);       int n,b,c,i;       n=sc.nextInt();       int[] a=new int[n];       for(i=0;i<n;i++)       {           a[i]=sc.nextInt();       }       b=sc.nextInt();       c=sc.nextInt();       int min=99999999;       for(i=0;i<n;i++)       {           if(a[i]>=b && a[i]<=c && a[i]<min)           {               min=a[i];           }       }       if(min==99999999)       System.out.println("-1");       else        System.out.println(min);     } }

---
+++ Minimum_between_A_and_B.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Codemind
+{
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      int n,b,c,i;
+      n=sc.nextInt();
+      int[] a=new int[n];
+      for(i=0;i<n;i++)
+      {
+          a[i]=sc.nextInt();
+      }
+      b=sc.nextInt();
+      c=sc.nextInt();
+      int min=99999999;
+      for(i=0;i<n;i++)
+      {
+          if(a[i]>=b && a[i]<=c && a[i]<min)
+          {
+              min=a[i];
+          }
+      }
+      if(min==99999999)
+      System.out.println("-1");
+      else
+       System.out.println(min);
+    }
+}
